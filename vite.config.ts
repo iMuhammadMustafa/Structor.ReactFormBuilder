f4b9d43eb576c12a4786f5339a322286f30493e4
iMuhammadMustafa <imohamedmostafa95@gmail.com>
2022-10-17 17:47:43 +0200

    Add .env functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    //envPrefix: "VITE",
+    server: {
+      host: true,
+      port: Number.parseInt(env.PORT) || 3000,
+      open: true,
+    },
+  };
+});
+
+// export default ({ mode }) => {
+//   // Extends 'process.env.*' with VITE_*-variables from '.env.(mode=production|development)'
+//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+//   console.log(process.env.VITE_PORT);
+//   return defineConfig({
+//     plugins: [react()],
+//   });
+// };
