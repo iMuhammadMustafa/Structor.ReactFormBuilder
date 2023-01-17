99be7566334a5f6714cebe2a88b6749ce596229e
iMuhammadMustafa <imohamedmostafa95@gmail.com>
2023-01-18 01:35:56 +0200

    Update InputGroup.jsx
    Fixed some issues with values and removed commented code.

--- src/Modules/FormBuilder/Components/Elements/InputGroup.jsx
+++ src/Modules/FormBuilder/Components/Elements/InputGroup.jsx
@@ -1,25 +1,19 @@
-import { useEffect } from "react";
-
 import FormBuilder from "../Form/FormBuilder";
 
 export default function InputGroup(props) {
-  const name = props.name;
-  const fields = props.fields;
-  const isFormSubmit = props.isFormSubmit;
-  const isFormCleared = props.isFormCleared;
-  const setIsFormTouched = props.setIsFormTouched;
-  const groupValues = props.value;
-  const handleInputChange = props.handleInputChange;
-  const handleInputBlur = props.handleInputBlur;
-  const errors = props.errors;
-  const label = props.label;
-  const setValues = props.setValues;
-
-  //   const handleGroupChange = async e => {
-  //     setValues({ ...groupValues, [name]: { [e.target.name]: e.target.value } });
-  //     await handleInputChange({ name: e.target.name, parent: name, value: e.target.value });
-  //   };
-  //   const handleGroupBlur = e => {};
+  const {
+    name,
+    fields,
+    isFormSubmit,
+    isFormCleared,
+    setIsFormTouched,
+    value,
+    handleInputChange,
+    handleInputBlur,
+    errors,
+    label,
+    setValues,
+  } = { ...props };
 
   const handleGroupBlur = async (field, e) => {
     await handleInputBlur({ ...field, parentName: name }, e);
@@ -27,11 +21,6 @@ export default function InputGroup(props) {
   const handleGroupChange = async (field, e) => {
     await handleInputChange({ ...field, parentName: name }, e);
   };
-
-  useEffect(() => {
-    console.log("Value of name");
-    console.log(groupValues);
-  }, [groupValues]);
 
   return (
     <div className="card bg-dark">
@@ -42,7 +31,7 @@ export default function InputGroup(props) {
           isFormSubmit={isFormSubmit}
           isFormCleared={isFormCleared}
           setIsFormTouched={setIsFormTouched}
-          values={groupValues ? groupValues : ""}
+          values={value ? value : ""}
           setValues={setValues}
           handleInputChange={handleGroupChange}
           handleInputBlur={handleGroupBlur}
@@ -52,10 +41,3 @@ export default function InputGroup(props) {
     </div>
   );
 }
-/*
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-        */
