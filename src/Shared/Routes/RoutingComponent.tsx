f1e8d1d2d63e010fec5884539dcccd727fff4ca8
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-09-30 13:10:34 -0500

    Remove everything.

--- src/Shared/Routes/RoutingComponent.tsx
+++
@@ -1,8 +0,0 @@
-import { useRoutes } from "react-router-dom";
-
-import { getRoutes } from "./Routing.Service";
-
-export default function RoutingComponent() {
-  const routes = getRoutes();
-  return useRoutes([...routes]);
-}
